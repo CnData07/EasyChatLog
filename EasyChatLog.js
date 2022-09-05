ffc6203e5fa7b9daae599e5ba86a1c0f6ca3616e
CnData07 <data07@126.com>
2022-09-05 17:29:46 +0800

    Update EasyChatLog.js

--- EasyChatLog.js
+++ EasyChatLog.js
@@ -1,7 +1,7 @@
 //LiteLoaderScript Dev Helper
 /// <reference path="G:/Minecraft/LXL/JS_Edit/HelperLib-master/src/index.d.ts"/> 
 
-const _VER = '1.0.1';
+const _VER = '1.0.2';
 //简单记录一下玩家聊天内容
 const dir_path = './plugins/EasyChatLog/';
 
